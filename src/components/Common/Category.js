aa5d7bd711b5544c8595a01778997de0110d9b05
jellysoo97 <jellysoo97@gmail.com>
2022-02-04 23:41:48 +0900

    카테고리 수정

--- src/components/Common/Category.js
+++ src/components/Common/Category.js
@@ -1,10 +1,22 @@
 import React from "react"
 
 function Category() {
+  const CateName = ({ link, name }) => {
+    return (
+      <a href={link} className="list-group-item list-group-item-secondary" style={{ padding: "0.8rem 1rem" }}>
+        {name}
+      </a>
+    )
+  }
+
   return (
-    // <div className="col-sm-3" style={{ height: "500px", backgroundColor: "#eee", paddingLeft: "50px", margin: "50px"}}>
-    <div style={{ height: "500px", backgroundColor: "#eee"}}>
-      게시판
+    <div style={{ height: "500px" }}>
+      <ul className="list-group">
+        <CateName link={"/postlist"} name={"IT Issue"} />
+        <CateName link={"#"} name={"Session"} />
+        <CateName link={"#"} name={"Study"} />
+        <CateName link={"#"} name={"Project"} />
+      </ul>
     </div>
   )
 }
