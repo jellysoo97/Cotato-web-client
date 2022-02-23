58adc88dfbd5305c2fe2622e9936d912eb197310
jellysoo97 <jellysoo97@gmail.com>
2022-02-23 18:39:41 +0900

    좋아요, 수정, 삭제 빼고 댓글 api

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -1,162 +1,66 @@
-import React, { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import React, { useState } from "react"
+import { Fragment } from "react"
 import axios from "axios"
 
+import SingleComment from "./SingleComment"
 import CommentReply from "./CommentReply"
 
 function Comment(props) {
-  const postNumber = useParams()
-  const [comments, setComments] = useState([])
-  const [input, setInput] = useState()
+  const postId = props.postId
+  const [commentValue, setcommentValue] = useState("")
+  const user = "userID"
 
-  const createComment = (e) => {
+  const handleChange = (e) => {
+    setcommentValue(e.target.value)
+  }
+
+  const onSubmit = (e) => {
     e.preventDefault()
 
     const variable = {
-      content: input,
-      postId: postNumber,
+      postId: postId,
+      username: user,
+      text: commentValue,
     }
 
-    axios.post("http://localhost:8080/comment/" + postNumber + "/createComment ", variable).then((response) => {
+    axios.post("http://localhost:8080/comment/" + postId + "/getComment", variable).then((response) => {
       if (response.data) {
-        //console.log(response.data.result);
-        setComments("")
-
-        //PostPageView(부모)의 Comments를 수정해줘야 함
-        props.refreshFunc(response.data.result)
+        console.log(response.data)
       } else {
-        alert("댓글 등록 실패")
+        alert("댓글 저장 실패")
       }
     })
-  }
-
-  const updateComment = (e) => {
-    e.preventDefault()
-
-    const variable = {
-      content: input,
-    }
-
-    axios.put("http://localhost:8080/comment/" + postNumber + "/updateComment ", variable).then((response) => {
-      if (response.data) {
-        //console.log(response.data.result);
-        setComments("")
-
-        //PostPageView(부모)의 Comments를 수정해줘야 함
-        props.refreshFunc(response.data.result)
-      } else {
-        alert("댓글 수정 실패")
-      }
-    })
-  }
-
-  const deleteComment = (e) => {
-    e.preventDefault()
-
-    axios.put("http://localhost:8080/comment/" + postNumber + "/createComment ").then((response) => {
-      if (response.data) {
-        //console.log(response.data.result);
-        setComments("")
-
-        //PostPageView(부모)의 Comments를 수정해줘야 함
-        props.refreshFunc(response.data.result)
-      } else {
-        alert("댓글 삭제 실패")
-      }
-    })
-  }
-
-  const onChange = (e) => {
-    setInput(e.target.value)
-  }
-
-  useEffect(() => {
-    async function getComments() {
-      try {
-        const response = await axios.get("http://localhost:8080/comment/" + postNumber + "/getComment")
-        setComments(response.data)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    getComments()
-  }, [])
-
-  const addComment = () => {
-    setComments(
-      comments.concat({
-        postId: postNumber,
-        id: comments.length + 1,
-        name: "userID",
-        date: new Date().toISOString().slice(0, 10),
-        content: input,
-      })
-    )
-    setInput("")
-    createComment()
-  }
-
-  const removeComment = (id) => {
-    setComments(comments.filter((comment) => comment.id !== id))
-    deleteComment()
-  }
-
-  const changeComment = (id, inputWord) => {
-    setComments(
-      comments.map((comment) => {
-        if (comment.id === id) {
-          return {
-            ...comment,
-            content: inputWord,
-          }
-        }
-        return comment
-      })
-    )
-    setInput("")
-    updateComment()
   }
 
   return (
-    <div className="container mt-4 p-4">
-      <div className="row mb-4">
-        <div className="col-12">댓글 {comments.length} 개</div>
-      </div>
-      {comments.map((comment, index) => {
-        return (
-          <div className="row mb-4 border-bottom border-secondary">
-            <div className="col-3 p-2 fw-bold">{comment.name}</div>
-            <div className="col-3 p-2">{comment.date}</div>
-            <div key={index} className="col-6 d-grid gap-2 d-flex justify-content-end">
-              <button className="btn btn-outline-warning">좋아요</button>
-              <button className="btn btn-outline-warning" onClick={() => changeComment(comment.id, input)}>
-                수정
-              </button>
-              <button className="btn btn-outline-warning" onClick={() => removeComment(comment.id)}>
-                삭제
-              </button>
-              <CommentReply />
-            </div>
-            <div className="col-12 p-3 mb-3">{comment.content}</div>
-          </div>
-        )
-      })}
+    <div className="container mt-4 p-4 border-top border-dark">
+      {/* Comment Lists */}
       <div className="row">
-        <div className="col-10">
-          <textarea className="form-control" value={input} onChange={onChange} placeholder={"댓글을 입력하세요"} rows="3"></textarea>
-        </div>
-        <div className="col-2">
-          <button
-            className="btn btn-outline-warning p-4"
-            onClick={() => {
-              addComment(input)
-              setInput("")
-            }}
-          >
-            댓글달기
-          </button>
-        </div>
+        {props.commentList &&
+          props.commentList.map(
+            (comment, index) =>
+              !comment.id && ( //대댓글은 우선 숨기겠다는 의미
+                <Fragment key={index}>
+                  <SingleComment refreshFunction={props.refreshFunction} comment={comment} postId={props.postId} key={index} />
+                  <CommentReply refreshFunction={props.refreshFunction} commentList={props.commentList} parentCommentId={comment._id} postId={props.postId} key={index} />
+                </Fragment>
+              )
+          )}
       </div>
+
+      {/* Comment Form */}
+      <form>
+        <div className="row">
+          <div className="col-10">
+            <textarea className="form-control" value={commentValue} onChange={handleChange} placeholder={"댓글을 입력하세요"} rows="3"></textarea>
+          </div>
+          <div className="col-2 p-2 text-center">
+            <button className="btn btn-outline-warning p-4" onClick={onSubmit}>
+              댓글달기
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
--- src/components/comments/CommentReply.js
+++ src/components/comments/CommentReply.js
@@ -1,21 +1,45 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import { Fragment } from "react"
+import SingleComment from "./SingleComment"
 
-import Comment from "./Comment"
+function CommentReply(props) {
+  const [ChildCommentNumber, setChildCommentNumber] = useState(0)
+  const [OpenReplyComments, setOpenReplyComments] = useState(false)
 
-function CommentReply() {
-  const [OpenReply, setOpenReply] = useState(false)
+  useEffect(() => {
+    let commentNumber = 0
+    props.commentList.map((comment, index) => {
+      if (comment.id === props.parentCommentId) {
+        commentNumber++
+      }
+    })
+    setChildCommentNumber(commentNumber)
+  }, [props.commentList]) //commentList가 바뀔때마다 실행이될 수 있도록해야됨
+  const renderReplyComment = (parentCommentId) =>
+    props.commentList.map((comment, index) => (
+      <Fragment key={index}>
+        {comment.id === parentCommentId && (
+          <div style={{ width: "80%", marginLeft: "40px" }}>
+            <SingleComment refreshFunction={props.refreshFunction} comment={comment} postId={props.postId} />
+            <CommentReply refreshFunction={props.refreshFunction} commentList={props.commentList} postId={props.postId} parentCommentId={comment._id} />
+          </div>
+        )}
+      </Fragment>
+    ))
 
-  const onClickReplyOpen = () => {
-    setOpenReply(!OpenReply)
+  const onHandleChange = () => {
+    setOpenReplyComments(!OpenReplyComments)
   }
-
   return (
-    <>
-      <button onClick={onClickReplyOpen} type="button" className="btn btn-outline-warning" style={{ float: "right" }}>
-        답글달기
-      </button>
-      {/* {OpenReply && <Comment />} */}
-    </>
+    <div>
+      {ChildCommentNumber > 0 && (
+        <p style={{ fontSize: "14px", margin: "0", color: "gray" }} onClick={onHandleChange}>
+          대댓글 {ChildCommentNumber} 개
+        </p>
+      )}
+      {OpenReplyComments && renderReplyComment(props.parentCommentId)}
+      {/*대댓글을 달때 눌리며 나오고 아니면숨긴상태*/}
+    </div>
   )
 }
 
